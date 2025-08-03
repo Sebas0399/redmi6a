d4b2fc79274073ab11244adc514a22d521dd4d47
Sebas0399 <antony.aguay@gmail.com>
2025-08-03 16:52:49 -0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sebas0399.github.io',
+  base: 'redmi6a',
   vite: {
     plugins: [tailwindcss()]
   }
